c71f0518a7012d001211f24f0741fa8b71f81302
Tony Arredondo <tony.arredondo5@gmail.com>
2020-11-29 03:23:11 -0600

    update formatting

--- tp2md.py
+++ tp2md.py
@@ -1,21 +1,23 @@
 import re
-readme_header = ["# CW-Camo-Progress\n",
-                 "Taskpaper lists for my Cold War camo progress\n", "\n\n", "# Current Progress\n"]
+readme_header = [
+	"# CW-Camo-Progress\n", "Taskpaper lists for my Cold War camo progress\n",
+	"\n\n", "# Current Progress\n"
+]
 h2 = re.compile('(^\\W+)?(.*):')
 unchecked = re.compile('(\\W+)?(-\\W)(.*)')
 done = re.compile('(\\W+)?(-\\W\[\\W\]\\W)(.*)(\\W@done)')
 
 
 def tptomarkdown(filename):
-    file = open(filename, "r")
-    filelines = file.readlines()
-    filelines[0] = h2.sub(r'## \2', filelines[0])
-    filestr = "".join(filelines)
-    filestr = h2.sub(r'### \2', filestr)
-    filestr = unchecked.sub(r'\n- [ ] \3', filestr)
-    filestr = done.sub(r'\n- [x] \3', filestr)
-    file.close()
-    return filestr
+	file = open(filename, "r")
+	filelines = file.readlines()
+	filelines[0] = h2.sub(r'## \2', filelines[0])
+	filestr = "".join(filelines)
+	filestr = h2.sub(r'### \2', filestr)
+	filestr = unchecked.sub(r'\n- [ ] \3', filestr)
+	filestr = done.sub(r'\n- [x] \3', filestr)
+	file.close()
+	return filestr
 
 
 dm_ultra = tptomarkdown("DM-Ultra.taskpaper")
@@ -29,3 +31,4 @@ readme.write(dm_ultra)
 readme.write('\n\n\n')
 readme.write(dark_aether)
 readme.close()
+
